285bfbe29d42cc38692788105fffbe7f4eef285e
lifesinger <lifesinger@gmail.com>
2010-11-01 14:22:48 +0800

    refactor dd

--- dd/drag.js
+++ dd/drag.js
@@ -1,107 +1,60 @@
 /**
- * dd support for kissy ,drag for dd
- * @author:yiminghe@gmail.com
+ * dd support for kissy, drag for dd
+ * @author: yiminghe@gmail.com
  */
-KISSY.add("dd-drag", function() {
-    var S = KISSY,
-        KE = S.Editor,
-        Event = S.Event,
-        UA = S.UA,
-        DOM = S.DOM,
-        Node = S.Node;
+KISSY.add('dd-draggable', function(S) {
 
-    var DDM = S.DD.DDM;
-
-    function normalElDom(el) {
-        return   el[0] || el;
-    }
-
-    function equals(e1, e2) {
-        //全部为空
-        if (!e1 && !e2)return true;
-        //一个为空，一个不为空
-        if (!e1 || !e2)return false;
-        e1 = normalElDom(e1);
-        e2 = normalElDom(e2);
-        return e1 === e2;
-    }
-
-    var unselectable =
-        UA.gecko ?
-            function(el) {
-                el = normalElDom(el);
-                el.style.MozUserSelect = 'none';
-            }
-            : UA.webkit ?
-            function(el) {
-                el = normalElDom(el);
-                el.style.KhtmlUserSelect = 'none';
-            }
-            :
-            function(el) {
-                el = normalElDom(el);
-                if (UA.ie || UA.opera) {
-                    var
-                        e,
-                        i = 0;
-
-                    el.unselectable = 'on';
-
-                    while (( e = el.all[ i++ ] )) {
-                        switch (e.tagName.toLowerCase()) {
-                            case 'iframe' :
-                            case 'textarea' :
-                            case 'input' :
-                            case 'select' :
-                                /* Ignore the above tags */
-                                break;
-                            default :
-                                e.unselectable = 'on';
-                        }
-                    }
-                }
-            };
-
+    var UA = S.UA;
 
     /*
      拖放纯功能类
      */
     function Draggable() {
-        var self = this;
-        Draggable.superclass.constructor.apply(self, arguments);
-        self._init();
+        Draggable.superclass.constructor.apply(this, arguments);
+        this._init();
     }
 
     Draggable.ATTRS = {
-        //拖放节点
-        node:{},
-        //handler 集合，注意暂时必须在 node 里面
-        handlers:{value:{}}
+        /**
+         * 拖放节点
+         */
+        node: { },
+
+        /**
+         * handler 集合，注意暂时必须在 node 里面
+         */
+        handlers:{ value: { } }
     };
 
     S.extend(Draggable, S.Base, {
-        _init:function() {
+
+        _init: function() {
             var self = this,
-                node = self.get("node"),
-                handlers = self.get("handlers");
-            //DDM.reg(node);
+                node = self.get('node'),
+                handlers = self.get('handlers');
+
             if (S.isEmptyObject(handlers)) {
                 handlers[node[0].id] = node;
             }
+
             for (var h in handlers) {
                 if (!handlers.hasOwnProperty(h)) continue;
-                var hl = handlers[h],ori = hl.css("cursor");
+                var hl = handlers[h],ori = hl.css('cursor');
                 if (!equals(hl, node)) {
-                    if (!ori || ori === "auto")
-                        hl.css("cursor", "move");
+                    if (!ori || ori === 'auto')
+                        hl.css('cursor', 'move');
+
                     //ie 不能被选择了
                     unselectable(hl);
                 }
             }
-            node.on("mousedown", self._handleMouseDown, self);
+
+            node.on('mousedown', self._handleMouseDown, self);
         },
-        _check:function(t) {
-            var handlers = this.get("handlers");
+
+        _check: function(t) {
+            var handlers = this.get('handlers');
+
             for (var h in handlers) {
                 if (!handlers.hasOwnProperty(h)) continue;
                 if (handlers[h].contains(t)
@@ -118,17 +71,17 @@ KISSY.add("dd-drag", function() {
          * 通知全局管理器开始作用
          * @param ev
          */
-        _handleMouseDown:function(ev) {
+        _handleMouseDown: function(ev) {
             var self = this,
-                t = new Node(ev.target);
+                t = new S.Node(ev.target);
             if (!self._check(t)) return;
             //chrome 阻止了 flash 点击？？
             if (!UA.webkit) {
                 //firefox 默认会拖动对象地址
                 ev.preventDefault();
             }
-            //
-            DDM._start(self);
+            
+            S.DD.DDM._start(self);
 
             var node = self.get("node"),
                 mx = ev.pageX,
@@ -146,7 +99,8 @@ KISSY.add("dd-drag", function() {
             self.set("diff", self._diff);
 
         },
-        _move:function(ev) {
+
+        _move: function(ev) {
             var self = this,
                 diff = self.get("diff"),
                 left = ev.pageX - diff.left,
@@ -157,10 +111,12 @@ KISSY.add("dd-drag", function() {
                 top:top
             });
         },
-        _end:function() {
+
+        _end: function() {
             this.fire("end");
         },
-        _start:function() {
+
+        _start: function() {
             this.fire("start");
         }
 
@@ -169,8 +125,50 @@ KISSY.add("dd-drag", function() {
     /*
      拖放实体，功能反应移动时，同时移动节点
      */
+    S.Draggable = Draggable;
 
 
-    S.DD.Draggable = Draggable;
+    function normalElDom(el) {
+        return el[0] || el;
+    }
+
+    function equals(e1, e2) {
+        if (!e1 && !e2) return true; // 全部为空
+        if (!e1 || !e2) return false; // 有一个为空
+        return normalElDom(e1) === normalElDom(e2);
+    }
+
+    var unselectable =
+        UA.gecko ?
+            function(el) {
+                el = normalElDom(el);
+                el.style.MozUserSelect = 'none';
+            }
+            : UA.webkit ?
+            function(el) {
+                el = normalElDom(el);
+                el.style.KhtmlUserSelect = 'none';
+            }
+            :
+            function(el) {
+                el = normalElDom(el);
+                if (UA.ie || UA.opera) {
+                    var e, i = 0;
+                    el.unselectable = 'on';
+
+                    while (( e = el.all[ i++ ] )) {
+                        switch (e.tagName.toLowerCase()) {
+                            case 'iframe' :
+                            case 'textarea' :
+                            case 'input' :
+                            case 'select' :
+                                /* Ignore the above tags */
+                                break;
+                            default :
+                                e.unselectable = 'on';
+                        }
+                    }
+                }
+            };
 
 });
--- dd/manager.js
+++ dd/manager.js
@@ -1,112 +1,67 @@
 /**
- * dd support for kissy ,manager for dd
- * @author:yiminghe@gmail.com
+ * dd support for kissy, manager for dd
+ * @author: yiminghe@gmail.com
  */
-KISSY.add("dd-manager", function() {
-    var S = KISSY,
-        Event = S.Event,
-        UA = S.UA,
-        DD_PG_ZINDEX = 999999,
+KISSY.add('dd-ddm', function(S) {
+
+    var Event = S.Event,
         DOM = S.DOM,
-        Node = S.Node;
+        Node = S.Node,
+        SHIM_ZINDEX = 999999;
 
+    S.DD = { };
 
-    /**
-     * Throttles a call to a method based on the time between calls. from YUI
-     * @method throttle
-     * @for KISSY
-     * @param fn {function} The function call to throttle.
-     * @param ms {int} The number of milliseconds to throttle the method call. Defaults to 150
-     * @return {function} Returns a wrapped function that calls fn throttled.
-     */
-
-    /*! Based on work by Simon Willison: http://gist.github.com/292562 */
-
-    function throttle(fn, scope, ms) {
-        ms = ms || 150;
-
-        if (ms === -1) {
-            return (function() {
-                fn.apply(scope, arguments);
-            });
-        }
-
-        var last = (new Date()).getTime();
-
-        return (function() {
-            var now = (new Date()).getTime();
-            if (now - last > ms) {
-                last = now;
-                fn.apply(scope, arguments);
-            }
-        });
+    function DDM() {
+        DDM.superclass.constructor.apply(this, arguments);
+        this._init();
     }
 
-
-    S.DD = {};
-
-    function Manager() {
-        var self = this;
-        Manager.superclass.constructor.apply(self, arguments);
-        self._init();
-    }
-
-    Manager.ATTRS = {
+    DDM.ATTRS = {
         /**
-         * mousedown 后 buffer 触发时间,100毫秒
+         * mousedown 后 buffer 触发时间
          */
-        timeThred:{value:200},
+        timeThred: { value: 200 },
+
         /**
-         * 当前激活的拖对象，在同一时间只有一个值，所以不是数组
+         * 当前激活的拖动对象，在同一时间只有一个值，所以不是数组
          */
-        activeDrag:{},
-        /**
-         * 所有注册对象
-         */
-        drags:{value:{}}
+        activeDrag: { }
     };
 
     /*
      负责拖动涉及的全局事件：
      1.全局统一的鼠标移动监控
      2.全局统一的鼠标弹起监控，用来通知当前拖动对象停止
-     3.为了跨越iframe而统一在底下的遮罩层
+     3.为了跨越 iframe 而统一在底下的遮罩层
      */
-    S.extend(Manager, S.Base, {
-        _init:function() {
+    S.extend(DDM, S.Base, {
+
+        _init: function() {
             var self = this;
-            self._showPgMove = throttle(self._move, self, 30);
+            self._showShimMove = throttle(self._move, self, 30);
         },
-        /*
-         注册所有可拖动对象
-         */
-        /*
-         reg:function(node) {
-         var drags = this.get("drags");
-         if (!node[0].id) {
-         node[0].id = S.guid("drag-");
-         }
-         drags[node[0].id] = node;
-         },*/
+
         /*
          全局鼠标移动事件通知当前拖动对象正在移动
-         注意：chrome8 :click 时 mousedown-mousemove-mouseup-click 也会触发 mousemove
+         注意：chrome8: click 时 mousedown-mousemove-mouseup-click 也会触发 mousemove
          */
-        _move:function(ev) {
-            var activeDrag = this.get("activeDrag");
-            //S.log("move");
-            //防止ie选择到字
+        _move: function(ev) {
+            var activeDrag = this.get('activeDrag');
+
+            //防止 ie 选择到字
             ev.preventDefault();
+
             if (!activeDrag) return;
             activeDrag._move(ev);
         },
+
         /**
          * 当前拖动对象通知全局：我要开始啦
          * 全局设置当前拖动对象，
-         * 还要根据配置进行buffer处理
+         * 还要根据配置进行 buffer 处理
          * @param drag
          */
-        _start:function(drag) {
+        _start: function(drag) {
             var self = this,
                 timeThred = self.get("timeThred") || 0;
 
@@ -122,40 +77,44 @@ KISSY.add("dd-manager", function() {
                 self._bufferStart(drag);
             }
         },
-        _bufferStart:function(drag) {
-            //S.log("_bufferStart");
+
+        _bufferStart: function(drag) {
             var self = this;
-            self.set("activeDrag", drag);
+            self.set('activeDrag', drag);
+
             //真正开始移动了才激活垫片
-            self._activePg();
+            self._activeShim();
             drag._start();
         },
+
         /**
          * 全局通知当前拖动对象：你结束拖动了！
          * @param ev
          */
-        _end:function(ev) {
+        _end: function(ev) {
             var self = this,
                 activeDrag = self.get("activeDrag");
-            //self._unregisterEvent();
+
             if (self._timeThredTimer) {
                 clearTimeout(self._timeThredTimer);
                 self._timeThredTimer = null;
             }
-            self._pg && self._pg.css({
+            self._shim && self._shim.css({
                 display:"none"
             });
+
             if (!activeDrag) return;
             activeDrag._end(ev);
             self.set("activeDrag", null);
         },
+
         /**
          * 垫片只需创建一次
          */
-        _activePg:function() {
+        _activeShim: function() {
             var self = this,doc = document;
             //创造垫片，防止进入iframe，外面document监听不到 mousedown/up/move
-            self._pg = new Node("<div " +
+            self._shim = new Node("<div " +
                 "style='" +
                 //red for debug
                 "background-color:red;" +
@@ -165,17 +124,17 @@ KISSY.add("dd-manager", function() {
                 "top:0;" +
                 "z-index:" +
                 //覆盖iframe上面即可
-                DD_PG_ZINDEX
+                SHIM_ZINDEX
                 + ";" +
                 "'></div>").appendTo(doc.body);
             //0.5 for debug
-            self._pg.css("opacity", 0);
-            self._activePg = self._showPg;
+            self._shim.css("opacity", 0);
+            self._activeShim = self._showShim;
         },
 
-        _showPg:function() {
+        _showShim: function() {
             var self = this;
-            self._pg.css({
+            self._shim.css({
                 display: "",
                 height: DOM.docHeight()
             });
@@ -193,25 +152,51 @@ KISSY.add("dd-manager", function() {
         /**
          * 开始时注册全局监听事件
          */
-        _registerEvent:function() {
+        _registerEvent: function() {
             var self = this,doc = document;
             //S.log("_registerEvent");
             Event.on(doc, "mouseup", self._end, self);
-            Event.on(doc, "mousemove", self._showPgMove, self);
+            Event.on(doc, "mousemove", self._showShimMove, self);
         },
 
         /**
          * 结束时需要取消掉，防止平时无谓的监听
          */
-        _unregisterEvent:function() {
+        _unregisterEvent: function() {
             var self = this,doc = document;
             //S.log("_unregisterEvent");
-            Event.remove(doc, "mousemove", self._showPgMove, self);
+            Event.remove(doc, "mousemove", self._showShimMove, self);
             Event.remove(doc, "mouseup", self._end, self);
         }
-
-
     });
 
-    S.DD.DDM = new Manager();
+    S.DD.DDM = new DDM();
+
+    /**
+     * Throttles a call to a method based on the time between calls. from YUI
+     * @method throttle
+     * @for KISSY
+     * @param fn {function} The function call to throttle.
+     * @param ms {int} The number of milliseconds to throttle the method call. Defaults to 150
+     * @return {function} Returns a wrapped function that calls fn throttled.
+     * ! Based on work by Simon Willison: http://gist.github.com/292562
+     */
+    function throttle(fn, scope, ms) {
+        ms = ms || 150;
+
+        if (ms === -1) {
+            return (function() {
+                fn.apply(scope, arguments);
+            });
+        }
+
+        var last = S.now();
+        return (function() {
+            var now = S.now();
+            if (now - last > ms) {
+                last = now;
+                fn.apply(scope, arguments);
+            }
+        });
+    }
 });
